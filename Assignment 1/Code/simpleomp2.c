d190708f7d2784f767706cf8a85c7a85707a864b
Bhuvaneswar D <80323398+Bhuvaneswar005@users.noreply.github.com>
2021-08-08 10:49:32 +0300

    C code for all.

---
+++ Assignment 1/Code/simpleomp2.c
@@ -0,0 +1,17 @@
+/*simpleomp.c*/
+#include<stdio.h>
+#include<omp.h>
+int main(){
+int nthreads,tid;
+omp_set_num_threads(8);
+#pragma omp parallel private(tid)
+{
+tid=omp_get_thread_num();
+printf("Hello world from thread=%d\n",tid);
+if(tid==0)
+{
+  nthreads=omp_get_num_threads();
+printf("Number of threads=%d\n",nthreads);
+}
+}
+}
